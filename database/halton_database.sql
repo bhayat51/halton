7d509e49a80bf886a0b814a3d7b87614c12cfc35
Joshua Prendergast <j.prendo10@gmail.com>
2014-03-06 10:03:36 +0000

    Update database schema

--- database/halton_database.sql
+++ database/halton_database.sql
@@ -1,6 +1,7 @@
 #
 # Creates the Halton Energy database
-# Run with mysql -u [user] -p -vvv < schema.sql
+# Run with mysql -u [user] -p -vvv < halton_database.sql
+#
 
 DROP DATABASE IF EXISTS halton_energy; # Uncomment to replace existing databases (dangerous)
 CREATE DATABASE halton_energy;
@@ -17,9 +18,12 @@ CREATE TABLE statistic (
 
 CREATE TABLE appliance (
     appliance_id VARCHAR(255) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    imageurl VARCHAR(250),
+    name VARCHAR(100) NOT NULL,
     description VARCHAR(256),
+    model VARCHAR(100),
+    size VARCHAR(40),
+    annualconsumption VARCHAR(150),
+    imageurl VARCHAR(250),
     latitude DECIMAL(10, 8),
     longitude DECIMAL(11, 8),
     PRIMARY KEY(appliance_id)
@@ -28,7 +32,7 @@ CREATE TABLE appliance (
 CREATE TABLE uses (
     use_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     appliance_id VARCHAR(255) NOT NULL,
-    name VARCHAR(50),
+    name VARCHAR(100),
     description VARCHAR(256),
     used INTEGER UNSIGNED NOT NULL,
     PRIMARY KEY(use_id),
