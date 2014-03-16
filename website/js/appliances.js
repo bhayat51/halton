7749903b6175bc06bf2b0b78aca1127dd82e9a88
Joshua Prendergast <j.prendo10@gmail.com>
2014-03-16 20:53:48 +0000

    Add shortcut links

--- website/js/appliances.js
+++ website/js/appliances.js
@@ -1,29 +1,39 @@
 var applianceSelected;
 
-/* Called when an appliance is selected */
-$('#appliance-list > .list-group-item').click(function() {
-    event.stopPropagation();
-
-    var applianceId = $(this).attr("item_key");
-    applianceSelected = applianceId;
-
-    // Toggle active state
-    $('#appliance-list > .list-group-item').removeClass("active");
-    $(this).addClass("active");
-
-    $("#usages").load("/include/appliances_viewer/usages.php?" + applianceId, function() { // Load possible usages for this appliance
-        $('#usages-list > .list-group-item').click(function() { // On completion, set usage list click event
-            event.stopPropagation();
-
-            var usageId = $(this).attr("item_key");
-
-            // Toggle active state
-            $('#usages-list > .list-group-item').removeClass("active");
-            $(this).addClass("active");
-
-            $("#appliance_info").load("/include/appliances_viewer/appliance_info.php?appliance=" + applianceSelected + "&usage=" + usageId); // Load appliance info
-        });
+jQuery(function($) {
+    // Called when an appliance is selected
+    $('#appliance-list > .list-group-item').click(function() {
+        onApplianceClicked($(this).attr("item_key"))
     });
 
-    $("#appliance_info").load("/include/appliances_viewer/appliance_info.php?appliance=" + applianceId); // Load appliance info
+    // Check for an appliance ID in the query string
+    var defaultAppliance = window.location.search.substring(1);
+    if (defaultAppliance && defaultAppliance.length > 0) {
+        onApplianceClicked(defaultAppliance);
+    }
+
+    function onApplianceClicked(applianceId) {
+        event.stopPropagation();
+        applianceSelected = applianceId;
+
+        // Toggle active state
+        $('#appliance-list > .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $("#usages").load("/include/appliances_viewer/usages.php?" + applianceId, function() { // Load possible usages for this appliance
+            $('#usages-list > .list-group-item').click(function() { // On completion, set usage list click event
+                event.stopPropagation();
+
+                var usageId = $(this).attr("item_key");
+
+                // Toggle active state
+                $('#usages-list > .list-group-item').removeClass("active");
+                $(this).addClass("active");
+
+                $("#appliance_info").load("/include/appliances_viewer/appliance_info.php?appliance=" + applianceSelected + "&usage=" + usageId); // Load appliance info
+            });
+        });
+
+        $("#appliance_info").load("/include/appliances_viewer/appliance_info.php?appliance=" + applianceId); // Load appliance info
+    }
 });
